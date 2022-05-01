c765b8ddd7a0d48132ed4c5011732ee22c0988af
martinV86 <martin_V86@mail.ru>
2022-05-01 19:20:38 +0300

    HashMap & TreeSet

--- src/com/belhard/lesson10/Main.java
+++ src/com/belhard/lesson10/Main.java
@@ -1,8 +1,37 @@
 package com.belhard.lesson10;
 
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.TreeMap;
+import java.util.TreeSet;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+
+//        TreeMap
+
+        TreeSet<String>treeSet=new TreeSet<>();
+        treeSet.add("qwerti");
+        treeSet.add("hello");
+        treeSet.forEach(s-> System.out.println(s));
+        String str="Heelo";
+        System.out.println(str.hashCode());
+
+//        HashMap
+
+        HashMap<String,String>map=new HashMap<>();
+        map.put("key1","value1");
+        map.put("key2","value2");
+        map.put("key3","value3");
+        map.put("key2","value22");
+//
+        System.out.println(map.get("key2"));
+        map.entrySet().forEach(entry->{
+            System.out.println(entry.getKey()+" "+ entry.getValue());
+        });
+//        LinkedHashMap linkedHashMap=new LinkedHashMap<>(); //сохраняет последовательность
+
     }
 }
